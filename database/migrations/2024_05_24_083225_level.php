f77f7b5e9d4167d4244bdcba5613031b9ad388d7
K0ruru <nurfaizqalbi@gmail.com>
2024-05-27 20:27:04 +0700

    fix some stuff

--- database/migrations/2024_05_24_083225_level.php
+++ database/migrations/2024_05_24_083225_level.php
@@ -3,15 +3,23 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
 
 return new class extends Migration {
     public function up(): void
     {
         Schema::create('level', function (Blueprint $table) {
             $table->id('id_level');
-            $table->enum('level', ['admin', 'petugas', 'masyarakat']);
+            $table->enum('level', ['admin', 'petugas']);
         });
+
+        // Insert default data
+        DB::table('level')->insert([
+            ['level' => 'admin'],
+            ['level' => 'petugas'],
+        ]);
     }
+
     /**
      * Reverse the migrations.
      */
